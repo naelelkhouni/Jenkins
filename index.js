00f2ef1f7e67c20b102aa18fea6e26348b843c7f
nael khouni <naelelkhouni@gmail.com>
2018-12-21 20:31:44 +0100

    add 2018

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Jenkins, from lab devOps 2018 de Nael !')
+  response.send('Hello word !')
 })
 
 app.listen(app.get('port'), function() {
